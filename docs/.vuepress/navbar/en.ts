81a5f078a6e12d2dd78c60a7eea1ebfe8220a1bd
Masterain <i@irain.in>
2023-04-06 22:01:32 -0700

    Update navbar

--- docs/.vuepress/navbar/en.ts
+++ docs/.vuepress/navbar/en.ts
@@ -3,7 +3,7 @@ import { navbar } from "vuepress-theme-hope";
 export const enNavbar = navbar([
   "/en/",
   { text: "Partnerships", icon: "discover", link: "/en/partnership.md" },
-  "API.md",
+  "/en/API.md",
   {
     text: "Standards",
     icon: "proposal",
