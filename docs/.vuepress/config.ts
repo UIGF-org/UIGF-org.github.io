89242c1c8989d8e54f419cbbad4a891731de3150
Masterain <i@irain.in>
2023-08-24 17:16:50 -0700

    update docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,10 +10,6 @@ export default defineUserConfig({
 
   dest: "./dist",
 
-  head: [
-      ['script', {'type': 'text/javascript', 'src': '/blacklist.js'}]
-  ],
-
   locales: {
     "/en/": {
       lang: "en-US",
